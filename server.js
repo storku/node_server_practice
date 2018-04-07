9c8c290cc50086a47c564bfcecefc8cbf53ad327
storku <36249562+storku@users.noreply.github.com>
2018-04-07 10:17:33 -0400

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
 });
